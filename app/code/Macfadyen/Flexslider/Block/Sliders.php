a6165d6e14c49e30fb8eae7b52b8192484a4c2e5
Sangeetha Johnson <sangeethajohns@gmail.com>
2018-05-26 00:56:07 +0530

    Macfadyen_Flexslider -Magento2 module

---
+++ app/code/Macfadyen/Flexslider/Block/Sliders.php
@@ -0,0 +1,71 @@
+<?php
+/*
+ * Macfadyen_Flexslider
+
+ * @category   Macfadyen
+ * @package    Macfadyen_Flexslider
+ * @version    1.0.0
+ */
+namespace Macfadyen\Flexslider\Block;
+
+use \Magento\Framework\View\Element\Template;
+use \Magento\Framework\View\Element\Template\Context;
+use \Magento\Store\Model\ScopeInterface;
+use \Magento\Framework\View\Asset\Repository;
+
+class Sliders extends Template
+{
+    const XML_PATH_SLIDERS_ENABLE    = 'homesliders/general/enable';
+    const XML_PATH_SLIDERS_AUTOMATE  = 'homesliders/general/automate';
+    const XML_PATH_SLIDERS_SPEED     = 'homesliders/general/speed';
+    const XML_PATH_SLIDERS_DIRECTION = 'homesliders/general/direction';
+    const XML_PATH_SLIDERS_CONTROL   = 'homesliders/general/paging';
+    const XML_PATH_SLIDERS_PAUSE     = 'homesliders/general/pause';
+    const XML_PATH_SLIDERS_LINK      = 'homesliders/general/link';
+
+    protected $_assetRepo;
+
+    /**
+     * Sliders constructor.
+     *
+     * @param Context $context
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Repository $assetRepo,
+        array $data = []
+    ) {
+        $this->_assetRepo = $assetRepo;
+        parent::__construct($context, $data);
+        
+    }
+
+    /**
+     * Check the module is enabled in the admin
+     *
+     * @return bool
+     */
+    public function isEnabled()
+    {
+        return $this->_scopeConfig->getValue(self::XML_PATH_SLIDERS_ENABLE, ScopeInterface::SCOPE_STORE);
+    }
+
+    /**
+     * Get the slider image URL
+     *
+     * @param $image
+     * @return bool|string
+     */
+    public function getImageUrl($image)
+    {
+        $url = '';
+        if ($image) {
+            if (is_string($image)) {
+
+                $url = $this->_assetRepo->getUrl("Macfadyen_Flexslider::images/$image");
+            }
+        }
+        return $url;
+    }
+}
---
+++ app/code/Macfadyen/Flexslider/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Macfadyen_Flexslider',
+    __DIR__
+);
